ebf770df2ee23af881f533d6fce458adbc6a37fd
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-23 23:47:02 +0530

    Create sidebar.jsx

---
+++ src/screens/global/sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+    return (
+        <div>
+            <div>
+                <h1>Sidebar</h1>
+            </div>
+        </div>
+        
+    );
+}
+export default Sidebar;
