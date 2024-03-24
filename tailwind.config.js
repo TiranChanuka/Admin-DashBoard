cdb35fb38c830c804cbe62e79cab5571d0da41a7
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-24 23:18:31 +0530

    add color,fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        "30px": "30px",
+        "40px": "40px",
+        "25px": "25px",
+        "17px": "17px",
+        "14px": "14px",
+        "13px": "13px",
+      },
+      fontWeight: {
+        thin: "100",
+        light: "300",
+        regular: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        black: "900",
+      },
+      colors: {
+        primary: "#101010",
+        textPrimary: "#FFFFFF",
+        green: "#31BA96",
+        iconBlue: "#32A7E2",
+        iconPurple: "#B548C6",
+        iconOrange: "#FF8700",
+        iconRed: "#DC3434",
+        iconGreen: "#4BA83D",
+      },
+    },
   },
   plugins: [],
 };
