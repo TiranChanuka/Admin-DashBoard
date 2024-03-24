b62922a489fed6ddf72baef7e6d738dae9a4bd2a
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-24 23:17:17 +0530

    Update sidebar.jsx

--- src/screens/global/sidebar.jsx
+++ src/screens/global/sidebar.jsx
@@ -1,11 +1,22 @@
-const Sidebar = () => {
-    return (
-        <div>
-            <div>
-                <h1>Sidebar</h1>
-            </div>
-        </div>
-        
-    );
-}
-export default Sidebar;
+import React from 'react'
+
+export const SideBar = () => {
+  return (
+      <div className='flex flex-col text-white p-14 w-100 bg-slate-800 font-poppins'>
+          <div>
+              <img
+                alt="profile-user"
+                width="72px"
+                height="72px"
+                src="./Assets/profile.png"
+                className="rounded-lg"
+                />
+          </div>
+          <div className='flex-1 mt-3'>
+              <div className='text-30px font-semibold'>Samantha</div>
+              <div className='text-17px'>samantha@email.com</div>
+          </div>
+          
+    </div>
+  )
+} 
