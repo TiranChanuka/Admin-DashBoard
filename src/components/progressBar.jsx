52532ccf3bef968d3ddef0f8418076b2a3955198
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-27 21:57:18 +0530

    Create progressBar.jsx

---
+++ src/components/progressBar.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export const ProgressBar = () => {
+    return (
+        <div>
+            <div className='mt-8  '>
+                <div className=' flex justify-between'>
+                    <div className='flex'>
+                        <div className=''>
+                            <div className='text-13px font-medium'>Food and Drinks</div>
+                        </div>
+                    </div>
+                    <div className='text-13px font-medium'>872.400</div>
+                </div>
+                <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
+                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                </div>
+            </div><div className='mt-8  '>
+                <div className=' flex justify-between'>
+                    <div className='flex'>
+                        <div className=''>
+                            <div className='text-13px font-medium'>Food and Drinks</div>
+                        </div>
+                    </div>
+                    <div className='text-13px font-medium'>872.400</div>
+                </div>
+                <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
+                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                </div>
+            </div><div className='mt-8  '>
+                <div className=' flex justify-between'>
+                    <div className='flex'>
+                        <div className=''>
+                            <div className='text-13px font-medium'>Food and Drinks</div>
+                        </div>
+                    </div>
+                    <div className='text-13px font-medium'>872.400</div>
+                </div>
+                <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
+                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                </div>
+            </div><div className='mt-8  '>
+                <div className=' flex justify-between'>
+                    <div className='flex'>
+                        <div className=''>
+                            <div className='text-13px font-medium'>Food and Drinks</div>
+                        </div>
+                    </div>
+                    <div className='text-13px font-medium'>872.400</div>
+                </div>
+                <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
+                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                </div>
+            </div><div className='mt-8  '>
+                <div className=' flex justify-between'>
+                    <div className='flex'>
+                        <div className=''>
+                            <div className='text-13px font-medium'>Food and Drinks</div>
+                        </div>
+                    </div>
+                    <div className='text-13px font-medium'>872.400</div>
+                </div>
+                <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
+                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
