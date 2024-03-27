a14455bf66a85b7efd774a34b891604d29159bc3
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-27 21:57:30 +0530

    Update sideBarNew.jsx

--- src/screens/global/sideBarNew.jsx
+++ src/screens/global/sideBarNew.jsx
@@ -9,6 +9,9 @@ const linkClass =
 export default function Sidebar() {
     return (
         <div className=" w-100 p-14 flex flex-col">
+            <div className="transform -translate-y-1/4 absolute left-28">
+                <div className=" w-[29px] h-[29px] rounded-full bg-iconRed  text-center text-white">4</div>
+            </div>
             <div className="flex items-center gap-2 px-1 py-3">
                 <img
                     alt="profile-user"
@@ -17,13 +20,12 @@ export default function Sidebar() {
                     src="./Assets/profile.png"
                     className={`cursor-pointer duration-500 rounded-md`}
                 />
-                <div className="text-white  bg-iconRed font-poppins rounded-full p-2 text-13px absolute top-11 left-32">4</div>
             </div>
             <div className="text-white">
                 <div className="text-30px font-semibold">Samantha</div>
                 <div className="text-17px">samantha@email.com</div>
             </div>
-            <div className="py-8 pt-20 flex flex-1 flex-col gap-0.5">
+            <div className="py-8 pt-20 flex flex-1 flex-col gap-5 ">
                 {SideBarLinks.map((link) => (
                     <SidebarLink key={link.key} link={link} />
                 ))}
@@ -49,4 +51,4 @@ function SidebarLink({ link }) {
             {link.label}
         </Link>
     );
-}
+};
