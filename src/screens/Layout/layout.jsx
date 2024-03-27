86cfba8da1ff350521a61416fb26466deb2d6820
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-27 21:57:33 +0530

    Update layout.jsx

--- src/screens/Layout/layout.jsx
+++ src/screens/Layout/layout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from '../global/sideBarNew';
 
 const Layout = () => {
-  return (
-      <div className="bg-black h-screen w-screen overflow-hidden flex flex-row ">
+	return (
+		<div className="bg-black h-screen w-screen overflow-hidden flex flex-row ">
 			<Sidebar />
 			<div className="flex flex-col flex-1">
 				<div className="flex-1 p-4 min-h-0 overflow-auto">
@@ -12,7 +12,7 @@ const Layout = () => {
 				</div>
 			</div>
 		</div>
-      
-  )
+
+	)
 }
 export default Layout;
