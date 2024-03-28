9a33f943d42c5131e1271b4cbf01ebbfe17a4605
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-28 19:41:54 +0530

    Update layout.jsx

--- src/screens/Layout/layout.jsx
+++ src/screens/Layout/layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../global/sideBarNew';
+// import { SideBar } from '../global/sidebar';
 
 const Layout = () => {
 	return (
 		<div className="bg-black h-screen w-screen overflow-hidden flex flex-row ">
 			<Sidebar />
+			{/* <SideBar /> */}
 			<div className="flex flex-col flex-1">
 				<div className="flex-1 p-4 min-h-0 overflow-auto">
 					<Outlet />
