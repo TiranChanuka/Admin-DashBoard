f611c4cff17e07a0a6a3a8c83ab6328c14bd8d96
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-28 19:41:47 +0530

    Update dashboard.jsx

--- src/screens/dashboard/dashboard.jsx
+++ src/screens/dashboard/dashboard.jsx
@@ -1,9 +1,17 @@
+import TransactionDashBoardBarChart from "../../components/barChartDash";
+
 const Dashboard = () => {
     return (
-        <div className=" bg-slate-400 rounded-3xl">
-            <div className="grid grid-cols-12 gap-4">
-                <div className="md:col-span-8 text-white">Dashboards</div>
-                <div className="md:col-span-4 bg-red-300 text-white">Dashboards</div>
+        <div className=" bg-white rounded-3xl p-5 sm:p-14">
+            <div className="sm:grid sm:grid-cols-12 gap-4">
+                <div className="text-30px sm:text-40px font-semibold">Dashboards</div>
+                <div className="md:col-span-12 text-white">
+                    <div>
+                        <TransactionDashBoardBarChart />
+                    </div>
+                </div>
+                <div className="md:col-span-4 bg-red-300 text-white text-30px sm:text-40px font-semibold p-2">Text 1</div>
+                <div className="md:col-span-8 bg-iconPurple text-white text-30px sm:text-40px font-semibold p-2">Text 2</div>
             </div>
         </div>
 
