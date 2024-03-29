36a50e2c90ce99ccaa16c1423c29b7d00614821e
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-29 19:14:44 +0530

    Update layout.jsx

--- src/screens/Layout/layout.jsx
+++ src/screens/Layout/layout.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../global/sideBarNew';
+import FabButton from '../../components/floatingButton';
+import { CgMenuGridO } from "react-icons/cg";
+import Popup from 'reactjs-popup';
 // import { SideBar } from '../global/sidebar';
 
 const Layout = () => {
+	const handleClick = () => {
+		// Handle button click actions here
+		<Popup trigger={<button> Trigger</button>}>
+			<div>
+				<ul>
+					<li>hello</li>
+				</ul>
+			</div>
+		</Popup>
+
+	}
 	return (
 		<div className="bg-black h-screen w-screen overflow-hidden flex flex-row ">
-			{/* <Sidebar /> */}
+			<div className='hidden lg:block'>
+				<Sidebar />
+			</div>
+			<div className='block lg:hidden'>
+				<FabButton onClick={handleClick}>
+					<CgMenuGridO />
+				</FabButton>
+			</div>
+			{/* <div className=' block lg:hidden'>
+				<Sidebar />
+			</div> */}
+
 			{/* <SideBar /> */}
 			<div className="flex flex-col flex-1">
 				<div className="flex-1 p-4 min-h-0 overflow-auto">
