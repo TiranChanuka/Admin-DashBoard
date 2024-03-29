48231649b0dcbf86c70449dfbbf94c4fdf7f554c
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-29 21:31:02 +0530

    Update progressBar.jsx

--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -13,19 +13,19 @@ export const ProgressBar = () => {
                     <div className='text-13px font-medium'>872.400</div>
                 </div>
                 <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
-                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                    <div className='bg-green flex justify-center items-center w-[30%] h-full rounded-full'></div>
                 </div>
             </div><div className='mt-8  '>
                 <div className=' flex justify-between'>
                     <div className='flex'>
                         <div className=''>
-                            <div className='text-13px font-medium'>Food and Drinks</div>
+                            <div className='text-13px font-medium'>Shopping</div>
                         </div>
                     </div>
-                    <div className='text-13px font-medium'>872.400</div>
+                    <div className='text-13px font-medium'>1.378.200</div>
                 </div>
                 <div className='bg-progressBarBG h-1.5 rounded-full overflow-hidden mt-3'>
-                    <div className='bg-green flex justify-center items-center w-[70%] h-full rounded-full'></div>
+                    <div className='bg-green flex justify-center items-center w-[45%] h-full rounded-full'></div>
                 </div>
             </div><div className='mt-8  '>
                 <div className=' flex justify-between'>
